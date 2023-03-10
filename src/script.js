ea6c70a921a48716c93d5bf8545bac0aecc4a1ba
tanya <tanya16-99@mail.ru>
2023-03-10 16:10:05 +0300

    validation

---
+++ src/script.js
@@ -0,0 +1,39 @@
+let form = document.forms.form;
+let inputs = form.elements;
+
+form.addEventListener('click', function(event) {
+    let target = event.target;
+    
+    if (target.className.includes('name') || target.className.includes('textarea')) {
+        target.onblur = function() {
+            return validate(target, target.value.length);
+        };
+
+        target.onfocus = function() {
+            return removeError(target);
+        }
+    };
+
+    function validate(elem, lengtn) {
+        if (lengtn < 2) {
+            elem.classList.add('invalid');
+
+            let error = document.createElement('div');
+            error.className = 'error';
+            error.innerHTML = 'Введите хотя бы 2 символа';
+            elem.after(error);
+        };
+
+    };
+
+    function removeError(elem) {
+        if (elem.classList.contains('invalid')) {
+            let error = elem.nextSibling;
+            elem.classList.remove('invalid');
+            error.remove();
+        }
+    }
+    
+})
+
+
